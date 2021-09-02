765d29dc0e0754891d98711f73f25c89ee1788a1
Dev33Renan <85537259+Dev33Renan@users.noreply.github.com>
2021-09-01 22:47:13 -0300

    Betafuncionando

--- projetoblue/src/Cadastro.js
+++ projetoblue/src/Cadastro.js
@@ -12,6 +12,30 @@ export default function Cadastro() {
       "https://br.web.img3.acsta.net/pictures/21/05/26/20/19/2966501.jpg",
       
     },
+
+    {   
+      id: 2,  
+      nome: 'No Limite do Amanhã',
+      imageUrl: 
+      "https://www.hostfilmes.com/wp-content/uploads/2016/04/No-Limite-do-Amanh%C3%A3-1080p.jpg",
+      
+    },
+
+    {   
+      id: 3,  
+      nome: 'Guerra dos Mundos',
+      imageUrl: 
+      "https://media.fstatic.com/mz1mvqiQdyfSJJxDlU24st9qEp0=/fit-in/290x478/smart/media/movies/covers/2015/10/guerra-dos-mundos_t519.png",
+      
+    },
+
+    {   
+      id: 4,  
+      nome: 'Missão Impossível',
+      imageUrl: 
+      "https://th.bing.com/th/id/R.8523683e8373e2335b6bedb61c16b639?rik=bYlSlUB5%2bJr9vA&pid=ImgRaw&r=0",
+      
+    },
     
   ]);
   
@@ -82,10 +106,10 @@ export default function Cadastro() {
 
   return (
     <div>
-      <h1>Filmes</h1>
+      <h1>Catalogo de Filmes</h1>
       <h2>Cadastre um novo filme</h2>
-        <form onSubmit={handleSubmit}>
-            <label>Digite o nome:</label>
+        <form class="container" onSubmit={handleSubmit}>
+            <label>Digite o nome do filme:</label>
             <input type="text" value={nomeFilme} onChange={handleNomeChange}/>
             <br/>
             <label>Digite a url do filme:</label>
@@ -96,14 +120,17 @@ export default function Cadastro() {
         <ul>
             {filmes.map((filme, indice) => (
             <li key={indice}>
-                <p>
+              <p>
                 { filme.nome }
-                </p>
+              </p>
+              <div class="Container">
                 <p>
-                  <img class="img"src={filme.imageUrl} alt={filme.nome}/>
+                  <img src={filme.imageUrl} alt={filme.nome}/>
                 </p>
                 <button type="button" onClick={() => handleDelete(indice) }>Excluir</button>
                 <button type="button" onClick={() => {setAtualizando(true); setIdEdicao(indice);}}>Editar</button>
+              </div>
+               
             </li>
             ))}
                     
